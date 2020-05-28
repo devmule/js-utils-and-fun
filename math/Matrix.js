4faac439b5484b790760ad4589b8cc79628d8ad6
DevMule <35449804+DevMule@users.noreply.github.com>
2020-05-28 15:55:49 +0500

    det + inverted + jsdoc refactor + toString refactor

--- math/Matrix.js
+++ math/Matrix.js
@@ -4,7 +4,7 @@ class Matrix extends Array {
 	/** Return new matrix. Elements of new matrix are sum of elements of given matrices in same position.
 	 * @param {Matrix} m1
 	 * @param {Matrix} m2
-	 * @return Matrix
+	 * @return {Matrix}
 	 * **/
 	static add(m1, m2) {
 		return m1.clone().add(m2);
@@ -13,7 +13,7 @@ class Matrix extends Array {
 	/** Return new matrix. Elements of new matrix are difference between elements of given matrices in same position.
 	 * @param {Matrix} m1
 	 * @param {Matrix} m2
-	 * @return Matrix
+	 * @return {Matrix}
 	 * **/
 	static subtract(m1, m2) {
 		return m1.clone().subtract(m2);
@@ -22,7 +22,7 @@ class Matrix extends Array {
 	/** Return new matrix. Elements of new matrix are equal to elements of given matrices multiplied by given value.
 	 * @param {Matrix} m
 	 * @param {number} val
-	 * @return Matrix
+	 * @return {Matrix}
 	 * **/
 	static multiply(m, val) {
 		return m.clone().multiply(val);
@@ -31,16 +31,16 @@ class Matrix extends Array {
 	/** Multiply given matrices. Return new matrix as multiplication product.
 	 * @param {Matrix} m1
 	 * @param {Matrix} m2
-	 * @return Matrix
+	 * @return {Matrix}
 	 * **/
 	static dot(m1, m2) {
 		return m1.dot(m2);
 	}
 	
 	/**
-	 * @param {number|Array.<Array.<number>>} w
+	 * @param {(number|Array.<Array.<number>>)} w
 	 * @param {number} [h=0]
-	 * @return Matrix
+	 * @return {Matrix}
 	 * **/
 	constructor(w, h = 0) {
 		super();
@@ -60,14 +60,14 @@ class Matrix extends Array {
 	}
 	
 	/** Returns number of columns of matrix;
-	 * @return number
+	 * @return {number}
 	 * **/
 	get width() {
 		return this.length;
 	}
 	
 	/** Returns number of rows of matrix;
-	 * @return number
+	 * @return {number}
 	 * **/
 	get height() {
 		return this.length ? this[0].length : 0;
@@ -76,7 +76,7 @@ class Matrix extends Array {
 	
 	/** Read elements from given array. Return this matrix.
 	 * @param {Array.<Array.<number>>} array
-	 * @return Matrix
+	 * @return {Matrix}
 	 * **/
 	fromArray(array) {
 		if (!array.length || !array[0].length)
@@ -90,7 +90,7 @@ class Matrix extends Array {
 	
 	/** For each element in Matrix, set element to result of given function.
 	 * @param {function(number, int, int)} func - function(elem value, elem X, elem Y)
-	 * @return Matrix
+	 * @return {Matrix}
 	 * **/
 	forEach(func) {
 		for (let x = 0; x < this.width; x++)
@@ -101,7 +101,7 @@ class Matrix extends Array {
 	}
 	
 	/** Returns new Matrix with same values, or clone values to given Matrix instance.
-	 * @return Matrix
+	 * @return {Matrix}
 	 * **/
 	clone() {
 		return new Matrix(this);
@@ -110,7 +110,7 @@ class Matrix extends Array {
 	// simple operations
 	/** Add given matrix values to this matrix values. Return this matrix.
 	 * @param {Matrix} m
-	 * @return Matrix
+	 * @return {Matrix}
 	 * **/
 	add(m) {
 		if (this.width !== m.width || this.height !== m.height)
@@ -123,7 +123,7 @@ class Matrix extends Array {
 	
 	/** Subtract given matrix values from this matrix values. Return this matrix.
 	 * @param {Matrix} m
-	 * @return Matrix
+	 * @return {Matrix}
 	 * **/
 	subtract(m) {
 		if (this.width !== m.width || this.height !== m.height)
@@ -136,7 +136,7 @@ class Matrix extends Array {
 	
 	/** Multiply every element of Matrix by given value. Return this matrix.
 	 * @param {number} val
-	 * @return Matrix
+	 * @return {Matrix}
 	 * **/
 	multiply(val) {
 		this.forEach((e) => {
@@ -148,7 +148,7 @@ class Matrix extends Array {
 	
 	/** Multiply matrix with given matrix. Return new matrix as multiplication product.
 	 * @param {Matrix} m
-	 * @return Matrix
+	 * @return {Matrix}
 	 * **/
 	dot(m) {
 		if (this.width !== m.height)
@@ -166,7 +166,7 @@ class Matrix extends Array {
 	
 	// Matrix transformations
 	/** Return new matrix with swapped X and Y dimensions.
-	 * @return Matrix
+	 * @return {Matrix}
 	 * **/
 	get T() {
 		let m = new Matrix(this.height, this.width);
@@ -176,22 +176,81 @@ class Matrix extends Array {
 		return m;
 	}
 	
+	// lineal algebra
+	/** Return new inverted matrix. Matrix.dot(this, this.I) = matrix with ones on diagonals and zeros otherwise.
+	 * @return {(Matrix|null)}
+	 * **/
+	get I() {
+		// if det === 0, or matrix haven't det (det === null)
+		let d = this.det;
+		if (!d) return null; //throw new Error(`Determinant of matrix = 0, so it's cannot be inverted!`);
+		
+		let m = new Matrix(this.length, this.length); // matrix is square
+		
+		for (let x = 0; x < this.length; x++) {
+			for (let y = 0; y < this.length; y++) {
+				
+				// each cell value = (1/m.det) * (smaller_matrix.det) * (+1 or -1 depends on position)
+				
+				// get a smaller matrix
+				let a = new Matrix(this.length - 1, this.length - 1);
+				for (let ax = 0; ax < this.length - 1; ax++)  // for each X [from i+1, to i-1] (looped)
+					for (let ay = 0; ay < this.length - 1; ay++) // for each Y from [2 to end]
+						a[ax][ay] = this[ax + (ax >= x ? 1 : 0)][ay + (ay >= y ? 1 : 0)];
+				
+				// matrix is inverted, so coords of x and y are swapped
+				m[y][x] = 1 / d * a.det * ((x + y) % 2 ? -1 : 1);
+			}
+		}
+		
+		return m;
+	}
+	
+	/** Return determinant of this matrix if exist, otherwise return null.
+	 * @return {(number|null)}
+	 * **/
+	get det() {
+		// only square matrix can have det
+		if (this.width !== this.height) return null; // throw new Error(`Only square matrix can have det!`);
+		
+		let d = 0;
+		
+		// we've got square matrix, so (1×1) matrix's det is its value, and width == height == length
+		if (this.length === 1) return this[0][0];
+		if (this.length === 2) return this[0][0] * this[1][1] - this[1][0] * this[0][1];
+		
+		for (let i = 0; i < this.length; i++) { // associative element = this[i][0]
+			if (this[i][0]) { // if associative element == 0, then result == 0, we dont need to calculate
+				
+				let m = new Matrix(this.length - 1, this.length - 1); // new matrix with less dimensions
+				
+				for (let x = 0; x < this.length - 1; x++)  // for each X [from i+1, to i-1] (looped)
+					for (let y = 0; y < this.length - 1; y++) // for each Y from [2 to end]
+						m[x][y] = this[x + (x >= i ? 1 : 0)][y + 1];
+				
+				// associative elem * ±det
+				d += this[i][0] * m.det * (i % 2 ? -1 : 1);
+			}
+		}
+		return d;
+	}
+	
 	/** Change shape of matrix. Cut if width or height is bigger. expanded values become 0.
 	 * @param {number} width
 	 * @param {number} height
-	 * @return Matrix
+	 * @return {Matrix}
 	 * **/
 	reshape(width, height) {
 		if (width < 1 || height < 1)
 			throw new RangeError(`New shape must have at least 1 row and 1 col!`);
 		
-		while (this.length > width) this.pop();  // cut width if necessary
+		while (this.length > width) this.pop();  // cut width if need
 		for (let x = 0; x < width; x++) {
-			if (x >= this.length) this.push([]);  // add width if necessary
+			if (x >= this.length) this.push([]);  // add width if need
 			
-			while (this[x].length > height) this[x].pop();  // cut height if necessary
+			while (this[x].length > height) this[x].pop();  // cut height if need
 			for (let y = 0; y < height; y++) {
-				if (y >= this[x].length) this[x].push(0);  // add height if necessary
+				if (y >= this[x].length) this[x].push(0);  // add height if need
 			}
 		}
 		
@@ -205,14 +264,14 @@ class Matrix extends Array {
 	//
 	
 	/** Return pretty JSON string of this matrix array.
-	 * @return String
+	 * @return {String}
 	 * **/
 	toString() {
-		return JSON.stringify(this).split("],[").join("],\n[")
+		return JSON.stringify(this).split("],[").join("],\n [")
 	}
 	
 	/** Set every element of this matrix 1 if x == y, 0 otherwise. Return this matrix.
-	 * @return Matrix
+	 * @return {Matrix}
 	 * **/
 	diagonalOnes() {
 		this.forEach((e, x, y) => {
@@ -223,7 +282,7 @@ class Matrix extends Array {
 	
 	
 	/** Add random value from min to max to every element. Return this matrix.
-	 * @return Matrix
+	 * @return {Matrix}
 	 * **/
 	randomize(min = 0, max = 1) {
 		this.forEach((e) => {
